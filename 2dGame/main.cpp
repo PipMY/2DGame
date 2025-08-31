fe4823023b8e4e988e3127e3d6b3125aa84c4ff6
PipMY <pipmartinyates@icloud.com>
2025-08-31 22:00:46 +0100

    Adds player sprite and very basic movement

--- 2dGame/main.cpp
+++ 2dGame/main.cpp
@@ -1,5 +1,10 @@
 #include <SFML/Graphics.hpp>
 
+void movePlayer(sf::Sprite& playerSprite, sf::Vector2f offset) {
+    sf::Vector2f pos = playerSprite.getPosition();
+    playerSprite.setPosition({ pos.x + offset.x, pos.y + offset.y });
+}
+
 int main()
 {
     // create the window
@@ -12,6 +17,15 @@ int main()
 	circle.setFillColor(sf::Color(150,50, 250, 40));
 	circle.setPosition({ 375.0f, 275.0f });
 	octagon.setFillColor(sf::Color::Red);
+	sf::Texture playerTexture("Assets/Player/sheep.png");
+    sf::Sprite playerSprite(playerTexture);
+	playerSprite.setPosition({ 100.0f, 100.0f });
+	playerSprite.setScale({ 2.5f, 2.5f });
+	// This means it takes a rectangle starting at (0,0) with width 32 and height 32 of the image later on this will let us choose a different sprite on the sprite sheet in the case that the player would turn left or right
+	playerSprite.setTextureRect(sf::IntRect({ 0, 0 }, { 32, 32 }));
+	bool isMoving = false;
+
+    
     
 
 
@@ -28,10 +42,43 @@ int main()
         // clear the window with black colour
         window.clear(sf::Color::Black);
 
+        isMoving = false;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
+            playerSprite.setTextureRect(sf::IntRect({ 96, 96 }, { 32, 32 }));
+			movePlayer(playerSprite, { 1.0f, 0.0f });
+			isMoving = true;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        {
+            playerSprite.setTextureRect(sf::IntRect({ 64, 64 }, { 32, 32 }));
+            movePlayer(playerSprite, { -1.0f, 0.0f });
+            isMoving = true;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        {
+            playerSprite.setTextureRect(sf::IntRect({ 32, 32 }, { 32, 32 }));
+            movePlayer(playerSprite, { 0.0f, -1.0f });
+            isMoving = true;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        {
+            playerSprite.setTextureRect(sf::IntRect({ 0, 0 }, { 32, 32 }));
+            movePlayer(playerSprite, { 0.0f, 1.0f });
+            isMoving = true;
+        }
+        if (!isMoving) {
+            playerSprite.setTextureRect(sf::IntRect({ 0, 0 }, { 32, 32 }));
+        }
+        
+        
+
 		// ---------- DRAWING CODE GOES HERE ---------- //
 
         window.draw(circle);
 		window.draw(octagon);
+		window.draw(playerSprite);
 
         // end the current frame
         window.display();
